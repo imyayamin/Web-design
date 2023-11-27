b1da781d440563a33645cfc9fc00be42b396a5e0
imyayamin <131016914+imyayamin@users.noreply.github.com>
2023-11-27 14:31:46 -0300

    the marvels
    trabalho de web design - 3 trimestre

---
+++ main.js
@@ -0,0 +1,8 @@
+const btHamburger = document.querySelector(".menu-hamburger")
+const mainMenu = document.querySelector("nav.main-menu")
+
+btHamburger.addEventListener("click", () => {
+  btHamburger.classList.toggle("opened")
+  mainMenu.classList.toggle("opened")
+})
+
